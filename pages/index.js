e1afab411a2148dd3e57afae9ba9210eccdf8c2d
hariomvc <chaudharihariomvijay@gmail.com>
2023-09-17 22:36:20 +0530

    Other Components in Landing Page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ export default function Home() {
   return (
     <>
       <main
-        className={`${styles.main__div} flex flex-col justify-around gap-12`} id="mainDiv"
+        className={`${styles.main__div} flex flex-col justify-around gap-12`}
+        id="mainDiv"
       >
         <div className="strike">
           <span className="text-white text-6xl text-center font-normal">
@@ -54,6 +55,78 @@ export default function Home() {
           </span>
         </div>
       </main>
+      <section className={`${styles.section__2} flex flex-col py-16 gap-6`}>
+        <span className="text-4xl">
+          Say <span className="text-[#D33467]">I DO</span>
+          <br />
+          To Expert Wedding Planning!
+        </span>
+        <p className="w-1/3">
+          Are you ready to say ‘I Do’ to a wedding that exceeds your wildest
+          dreams? Our certified wedding planners are here to make it happen. Get
+          a free consultation and say goodbye to wedding planning stress and
+          hello to seamless perfection.{" "}
+        </p>
+        <input
+          type="text"
+          placeholder="NAME"
+          className="w-1/4 text-black bg-transparent border-0 border-b border-b-black outline-0 outline-0 placeholder:text-black"
+        />
+        <input
+          type="text"
+          placeholder="PHONE NO."
+          className="w-1/4 text-black bg-transparent border-0 border-b border-b-black outline-0 outline-0 placeholder:text-black"
+        />
+        <input
+          type="text"
+          placeholder="OTP"
+          className="w-1/4 text-black bg-transparent border-0 border-b border-b-black outline-0 outline-0 placeholder:text-black"
+        />
+        <button
+          type="submit"
+          className="w-1/4 rounded-full bg-black text-white py-2"
+        >
+          SUBMIT
+        </button>
+      </section>
+      <section className="flex flex-col gap-12 py-16 px-24">
+        <p className="text-[#D33467] flex font-medium gap-2">
+          <span className="text-6xl">THE BEST</span>
+          <span className="text-xl flex flex-col">
+            <span>IN</span>
+
+            <span>TOWN</span>
+          </span>
+          <span className="text-6xl">!</span>
+        </p>
+        <p className="text-center text-3xl">What Makes Wedsy Stand Out?</p>
+        <div className="flex flex-row gap-12 mx-auto">
+          <div className="text-center flex flex-col items-center gap-3">
+            <div className="bg-[#FFB8C0] flex justify-center rounded-3xl w-32 h-32">
+              <img className="m-auto" src="/assets/icons/easy.png" />
+            </div>
+            <span>Easy</span>
+          </div>
+          <div className="text-center flex flex-col items-center gap-3">
+            <div className="bg-[#D6FF79] flex justify-center rounded-3xl w-32 h-32">
+              <img className="m-auto" src="/assets/icons/price.png" />
+            </div>
+            <span>Unbeatable Pricing</span>
+          </div>
+          <div className="text-center flex flex-col items-center gap-3">
+            <div className="bg-[#F19A3E] flex justify-center rounded-3xl w-32 h-32">
+              <img className="m-auto" src="/assets/icons/quality.png" />
+            </div>
+            <span>Superior Quality</span>
+          </div>
+          <div className="text-center flex flex-col items-center gap-3">
+            <div className="bg-[#70D6FF] flex justify-center rounded-3xl w-32 h-32">
+              <img className="m-auto" src="/assets/icons/solutions.png" />
+            </div>
+            <span>Innovative Solutions</span>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
