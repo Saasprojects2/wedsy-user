7cb4d8352a21aa909d8b398c241245875c3992a0
hariomvc <chaudharihariomvijay@gmail.com>
2023-09-28 18:28:36 +0530

    Mobile View of Landing Page

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ export default function Home() {
   return (
     <>
       <main
-        className={`${styles.main__div} flex flex-col justify-around gap-12`}
+        className={`${styles.main__div} flex flex-col py-8 md:p-0justify-around gap-6 md:gap-12`}
         id="mainDiv"
       >
         <div className="strike">
-          <span className="text-white text-6xl text-center font-normal">
+          <span className="text-white text-2xl md:text-6xl text-center font-normal">
             WEDDINGS MADE EASY
           </span>
         </div>
         <div className="px-16 flex flex-row justify-between mb-16 text-white">
-          <div className="w-1/3  text-6xl ">
-            YOUR WEDDING <span className="text-8xl font-medium">MEGA</span>{" "}
-            STORE
+          <div className="w-1/3 text-4xl md:text-6xl ">
+            YOUR WEDDING{" "}
+            <span className="text-6xl md:text-8xl font-medium">MEGA</span> STORE
           </div>
-          <div className="flex flex-col w-1/3 text-center gap-6">
+          <div className="hidden md:flex flex-col w-1/3 text-center gap-6">
             <span className="text-3xl">YOUR WEDDING VISION, OUR EXPERTISE</span>
             <input
               type="text"
@@ -39,18 +39,28 @@ export default function Home() {
         </div>
       </main>
       <main className={`${styles.main__div__2}`}>
-        <img src="/assets/images/flowers-1.png" className=" float-left" />
-        <img src="/assets/images/flowers-2.png" className=" float-right" />
+        <img
+          src="/assets/images/flowers-1.png"
+          className="hidden md:inline float-left"
+        />
+        <img
+          src="/assets/images/flowers-2.png"
+          className="hidden md:inline float-right"
+        />
         <div className="text-center flex flex-col h-full">
-          <span className="text-[#622400] text-4xl mt-[15%]">
+          <span className="text-[#622400] text-2xl md:text-4xl py-6 md:py-0 md:mt-[15%]">
             DISCOVER WEDSY&#39;S MAGIC
           </span>
-          <span className="text-[#622400] text-8xl">MAGIC</span>
-          <span>MEMORIES MADE EASY, NO HASSLES TO SEE</span>
-          <span className="mt-16 text-left ml-8">
+          <span className="hidden md:block text-[#622400] md:text-8xl">
+            MAGIC
+          </span>
+          <span className="hidden md:block ">
+            MEMORIES MADE EASY, NO HASSLES TO SEE
+          </span>
+          <span className="hidden md:block mt-16 text-left ml-8">
             Share Your Desires - Where wedding dreams catch fire.
           </span>
-          <span className="text-sm mt-auto mb-4 ml-auto">
+          <span className="hidden md:block text-sm mt-auto mb-4 ml-auto">
             Let us bring your wedding vision to life. Your dream wedding, along
             with a free consultation, is just a click away! 🌟
           </span>
@@ -101,7 +111,7 @@ export default function Home() {
           <span className="text-6xl">!</span>
         </p>
         <p className="text-center text-3xl">What Makes Wedsy Stand Out?</p>
-        <div className="flex flex-row gap-12 mx-auto">
+        <div className="flex flex-col md:flex-row gap-12 mx-auto">
           <div className="text-center flex flex-col items-center gap-3">
             <div className="bg-[#FFB8C0] flex justify-center rounded-3xl w-32 h-32">
               <img className="m-auto" src="/assets/icons/easy.png" />
