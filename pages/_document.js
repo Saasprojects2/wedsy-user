247821170db6db57351c9a9af8875518d93f811e
hariomvc <chaudharihariomvijay@gmail.com>
2023-10-16 20:17:51 +0530

    Modified Ducoment to Add Whatsapp Bot

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,7 +7,13 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          type="text/javascript"
+          src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
+          id="aisensy-wa-widget"
+          widget-id="1GL46l"
+        ></script>
       </body>
     </Html>
-  )
+  );
 }
