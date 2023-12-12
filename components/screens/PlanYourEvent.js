834e041f230f48990427655e074e825ce6eb1407
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-12 23:24:57 +0530

    Modified PlanYourEvent, Updated Mobile UI

--- components/screens/PlanYourEvent.js
+++ components/screens/PlanYourEvent.js
@@ -4,22 +4,22 @@ import Link from "next/link";
 export default function PlanYourEvent() {
   return (
     <>
-      <section className="w-full px-16 py-12 ">
-        <p className="font-semibold text-4xl mb-12">
+      <section className="w-full p-6 md:px-16 py-12 ">
+        <p className="font-semibold text-2xl md:text-4xl mb-12">
           PLAN YOUR EVENT NOW WITH OUR NEW EVENT TOOL!
         </p>
         <div className="flex gap-6 relative">
-          <div className="flex flex-col gap-8 ">
-            <p className="text-xl w-2/3">
+          <div className="flex flex-col gap-2 md:gap-8 text-sm md:text-lg">
+            <p className="text-xl w-3/4">
               Explore the ease of planning with our event tool at Wedsy.
             </p>
-            <p className="text-xl w-2/3">
+            <p className="text-xl w-3/4">
               Utilize the tool to shortlist and choose your decorations
               effortlessly - all in one place, at Wedsy.
             </p>
             <Link
               href={`/event`}
-              className="bg-rose-900 rounded-full p-1 px-16 text-white w-max mx-auto mt-auto mb-12"
+              className="hidden md:inline bg-rose-900 rounded-full p-1 px-16 text-white w-max mx-auto mt-auto mb-12"
             >
               Try Now!
             </Link>
@@ -29,10 +29,28 @@ export default function PlanYourEvent() {
             alt="Decor"
             width={0}
             height={0}
-            sizes="20%"
+            sizes="100%"
             style={{ width: "20%", height: "auto", marginLeft: "auto" }}
+            className="hidden md:inline"
           />
+          <div className="inline md:hidden">
+            <Image
+              src="/assets/images/event-tool.png"
+              alt="Decor"
+              width={0}
+              height={0}
+              sizes="100%"
+              style={{ width: "100%" }}
+              className=""
+            />
+          </div>
         </div>
+        <Link
+          href={`/event`}
+          className="block md:hidden bg-rose-900 rounded-full p-1 px-16 text-white w-max mx-auto mt-4 md:mt-auto mb-4 md:mb-12"
+        >
+          Try Now!
+        </Link>
       </section>
     </>
   );
