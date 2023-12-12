c24abdda44602ad4ce38f5bc0f014f2b6ce9ffd3
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-12 18:03:52 +0530

    Updated User Sidebar

--- components/layout/UserSidebar.js
+++ components/layout/UserSidebar.js
@@ -1,6 +1,27 @@
 import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
 
 export default function UserSidebar({ display }) {
+  const [events, setEvents] = useState([]);
+  const fetchEvents = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/event`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setEvents(response);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+  useEffect(() => {
+    fetchEvents();
+  }, []);
   return (
     <Sidebar className="h-auto bg-rose-900" id="sidebar">
       <Sidebar.Items className="text-center">
@@ -30,7 +51,11 @@ export default function UserSidebar({ display }) {
             PAYMENTS
           </Sidebar.Item>
           <Sidebar.Item
-            href="/event"
+            href={
+              (events.length > 0 && events[0].eventDays?.length) > 0
+                ? `/event/${events[0]?._id}/planner`
+                : "/event"
+            }
             className={`text-white border-b border-white hover:bg-rose-900 rounded-none ${
               display === "events" && "font-semibold text-lg"
             }`}
