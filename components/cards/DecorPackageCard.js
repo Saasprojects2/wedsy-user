a2729a4c5c59d43b5361fc151357a3d51465b927
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-25 12:55:10 +0530

    Added DecorPackageCard

--- components/cards/DecorPackageCard.js
+++ components/cards/DecorPackageCard.js
@@ -1,38 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function DecorPackageCard({ className }) {
-  //   const { _id, thumbnail, name } = decor;
+export default function DecorPackageCard({ className, key, decorPackage }) {
+  const { _id, name } = decorPackage;
+  console.log(decorPackage);
   return (
     <>
       <Link
-        href={`/decor/view/`}
-        className={`p-6 shadow-md rounded-lg flex flex-col gap-2 ${className}`}
+        href={`/decor/packages/${_id}`}
+        className={`p-6 border bg-white shadow-md rounded-lg flex flex-col gap-2 ${className}`}
+        key={key}
       >
-        <div className={`relative grid grid-cols-2 gap-2`}>
-          {/* <Image
-            src={`${thumbnail}`}
-            alt="Decor"
-            // width={0}
-            // height={0}
-            sizes="100%"
-            layout={"fill"}
-            objectFit="cover"
-            // style={{ width: "100%", height: "auto" }}
-            className="rounded-xl"
-          /> */}
-          <div className="w-full h-32 bg-gray-400" />
-          <div className="w-full h-32 bg-gray-400" />
-          <div className="w-full h-32 bg-gray-400" />
-          <div className="w-full h-32 bg-gray-400" />
+        <div className={`grid grid-cols-2 gap-2`}>
+          {decorPackage.decor
+            .map((item) => item.thumbnail)
+            .map((item, index) => (
+              <div className="relative pt-[100%]" key={index}>
+                <Image
+                  key={index}
+                  src={`${item}`}
+                  alt="Decor"
+                  // width={0}
+                  // height={0}
+                  sizes="50%"
+                  layout={"fill"}
+                  objectFit="cover"
+                  // style={{ width: "100%", height: "auto" }}
+                  className="rounded-md"
+                />
+              </div>
+            ))}
         </div>
-        <p className="font-semibold">Silver package</p>
-        <p className="">Stage, Entrance, Nameboard</p>
+        <p className="font-semibold">{name}</p>
+        <p className="">
+          {decorPackage.decor.map((item) => item.category).join(", ")}
+        </p>
         <p className="text-xl text-rose-900 font-semibold">
-          ₹ 40,000
-          {/* {decor.productInfo.variant.artificialFlowers.sellingPrice ||
-            decor.productInfo.variant.mixedFlowers.sellingPrice ||
-            decor.productInfo.variant.naturalFlowers.sellingPrice} */}
+          ₹{" "}
+          {decorPackage.variant.artificialFlowers.sellingPrice ||
+            decorPackage.variant.mixedFlowers.sellingPrice ||
+            decorPackage.variant.naturalFlowers.sellingPrice}
         </p>
       </Link>
     </>
