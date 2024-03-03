7ce04ea767bb1a4444b100ac78c1f653d7824c0a
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-03 16:26:13 +0530

    Updated the Plan Your Event Component

--- components/screens/PlanYourEvent.js
+++ components/screens/PlanYourEvent.js
@@ -1,10 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./PlanYourEvent.module.css";
 
 export default function PlanYourEvent() {
   return (
     <>
-      <section className="w-full p-6 md:px-16 py-12 ">
+      <div className="hidden md:block mb-12">
+        <div
+          className={`${styles.bg_div_top} flex pb-8 flex-col justify-end px-24 `}
+        >
+          <p className="font-medium text-xl text-white uppercase">
+            Explore the ease of planning
+          </p>
+          <p className="font-semibold text-3xl text-white">
+            PLAN YOUR EVENT NOW WITH OUR NEW
+          </p>
+        </div>
+        <div
+          className={`${styles.bg_div_bottom} lex pt-8 flex-col justify-start px-24`}
+        >
+          <p className="font-bold text-6xl text-[#A00000]">EVENT TOOL!</p>
+          <div className="flex flex-row justify-between items-center">
+            <p className="font-medium text-xl text-[#A00000] w-1/2">
+              Utilize the tool to shortlist and choose your decorations
+              effortlessly - all in one place, at Wedsy.
+            </p>
+            <Link
+              href={`/event`}
+              className="bg-[#A00000] rounded-xl p-2 px-16 text-white w-max"
+            >
+              Try Now!
+            </Link>
+          </div>
+        </div>
+      </div>
+      <section className="w-full p-6 md:px-16 py-12 md:hidden">
         <p className="font-semibold text-2xl md:text-4xl mb-12">
           PLAN YOUR EVENT NOW WITH OUR NEW EVENT TOOL!
         </p>
