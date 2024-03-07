55c4331377e63dfb52088871523370b50788e23a
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-07 21:02:56 +0530

    Added EventHowItWorks Component

---
+++ components/screens/EventHowItWorks.js
@@ -0,0 +1,60 @@
+export default function EventHowItWorks() {
+  return (
+    <div className="px-12 md:px-48 py-8 flex flex-col gap-12">
+      <p className="font-semibold text-3xl md:text-center">How it works?</p>
+      <ol className="text-lg list-decimal flex flex-col gap-8">
+        <li>
+          <p>Name Your Event:</p>
+          <p>
+            Give your event a personal touch. Whether it&#39;s &#34;Kiara&#39;s
+            Wedding&#34; or &#34;RahulxKiara,&#34; your event starts with a name
+            that reflects your style.
+          </p>
+        </li>
+        <li>
+          <p> Select the Community:</p>
+          <p>
+            Choose your community to tailor the planning process to your
+            specific rituals and traditions. If you are uncomfortable, You can
+            always skip it
+          </p>
+        </li>
+        <li>
+          <p>Add Events:</p>
+          <p>
+            Break down your main event into its unique components - Haldi,
+            Sangeet, Wedding etc. . Add events one by one for us to capture the
+            essence of each celebration.
+          </p>
+        </li>
+        <li>
+          <p>Set Date and Time:</p>
+          <p>
+            Specify when each event will take place. This helps us understand
+            the flow and timing of your festivities.
+          </p>
+        </li>
+        <li>
+          <p>Enter Venue Details:</p>
+          <p>
+            Provide the name of the venue for each event. Understanding the
+            logistics ensures a smooth execution of your plans.
+          </p>
+        </li>
+        <li>
+          <p> Multiple events:</p>
+          <p>
+            If you have more than one sub event, easily include more by clicking
+            the &#34;Add day&#34; button after you have added your first event.
+          </p>
+        </li>
+      </ol>
+      <p className="text-lg">
+        Now, let&#39;s add the magic! Choose decor and services to make each
+        event uniquely yours. Whether you take charge or let your wedding
+        planner guide you, Wedsy&#39;s event tool ensures every detail aligns
+        with your vision.
+      </p>
+    </div>
+  );
+}
