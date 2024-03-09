177bcb85c32ee1db21c9452da9bdc5b0ad765181
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-09 20:33:56 +0530

    Updated My Account Page

--- pages/my-account.js
+++ pages/my-account.js
@@ -1,3 +1,4 @@
+import UserProfileHeader from "@/components/layout/UserProfileHeader";
 import UserSidebar from "@/components/layout/UserSidebar";
 import { checkValidEmail } from "@/utils/email";
 import { processMobileNumber } from "@/utils/phoneNumber";
@@ -78,13 +79,13 @@ export default function Account({ user }) {
   }, []);
   return (
     <>
-      <div className="flex flex-row">
-        <UserSidebar display={"my-account"} />
-        <div className="flex flex-col gap-3 p-12 flex-grow">
+      <div className="flex flex-col bg-gray-100">
+        <UserProfileHeader display={"my-account"} />
+        <div className="flex flex-col gap-3 px-8 md:px-36 mb-12 md:my-12">
           <p className="text-2xl font-medium">Personal Information</p>
-          <div className="flex flex-row gap-12 border-b border-black pb-4">
-            <div className="h-64 w-64 bg-gray-500 rounded-full" />
-            <div className="flex flex-col gap-3 flex-grow">
+          <div className="flex flex-col md:flex-row gap-8 md:gap-12 md:items-center mb-8">
+            <div className="h-64 w-64 bg-gray-500 rounded-full mx-auto md:mx-0" />
+            <div className="flex flex-col gap-3 md:flex-grow md:bg-white md:shadow-lg md:rounded-3xl md:p-8">
               <div>
                 <p>Full Name:</p>
                 <TextInput
@@ -97,7 +98,7 @@ export default function Account({ user }) {
                   className="w-full"
                 />
               </div>
-              <div className="grid grid-cols-2 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
                   <p>Email:</p>
                   <TextInput
@@ -128,9 +129,9 @@ export default function Account({ user }) {
                   />
                 </div>
               </div>
-              <div>
+              <div className="w-full md:w-auto">
                 <Button
-                  className="bg-rose-800 enabled:hover:bg-rose-900"
+                  className="bg-rose-800 enabled:hover:bg-rose-900 mx-auto md:mx-0 rounded-full md:rounded-lg"
                   onClick={() =>
                     editProfile ? updateProfile() : setEditProfile(true)
                   }
@@ -148,108 +149,116 @@ export default function Account({ user }) {
               </div>
             </div>
           </div>
-          <p className="text-2xl font-medium">Home Address</p>
-          <div className="grid grid-cols-2 gap-12 gap-y-4">
-            <div>
-              <p>Apartment/House number:</p>
-              <TextInput
-                value={userInfo.address?.apartment}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, apartment: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <p>Street/Locality:</p>
-              <TextInput
-                value={userInfo.address?.street}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, street: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <p>City:</p>
-              <TextInput
-                value={userInfo.address?.city}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, city: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <p>State:</p>
-              <TextInput
-                value={userInfo.address?.state}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, state: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <p>Landmark:</p>
-              <TextInput
-                value={userInfo.address?.landmark}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, landmark: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <p>PinCode:</p>
-              <TextInput
-                value={userInfo.address?.pinCode}
-                onChange={(e) => {
-                  setUserInfo({
-                    ...userInfo,
-                    address: { ...userInfo.address, pinCode: e.target.value },
-                  });
-                }}
-                readOnly={!editAddress}
-                disabled={!editAddress}
-                className="w-full"
-              />
-            </div>
-            <div>
-              <Button
-                className="bg-rose-800 enabled:hover:bg-rose-900"
-                onClick={() =>
-                  editAddress ? updateAddress() : setEditAddress(true)
-                }
-                disabled={loading}
-              >
-                {editAddress ? "Update Address" : "Edit Address"}
-              </Button>
+          <div className="md:bg-white md:shadow-lg md:rounded-3xl md:p-8">
+            <p className="text-xl font-medium mb-3">Home Address</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 gap-y-4">
+              <div>
+                <p>Apartment/House number:</p>
+                <TextInput
+                  value={userInfo.address?.apartment}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: {
+                        ...userInfo.address,
+                        apartment: e.target.value,
+                      },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <p>Street/Locality:</p>
+                <TextInput
+                  value={userInfo.address?.street}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: { ...userInfo.address, street: e.target.value },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <p>City:</p>
+                <TextInput
+                  value={userInfo.address?.city}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: { ...userInfo.address, city: e.target.value },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <p>State:</p>
+                <TextInput
+                  value={userInfo.address?.state}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: { ...userInfo.address, state: e.target.value },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <p>Landmark:</p>
+                <TextInput
+                  value={userInfo.address?.landmark}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: {
+                        ...userInfo.address,
+                        landmark: e.target.value,
+                      },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <p>PinCode:</p>
+                <TextInput
+                  value={userInfo.address?.pinCode}
+                  onChange={(e) => {
+                    setUserInfo({
+                      ...userInfo,
+                      address: { ...userInfo.address, pinCode: e.target.value },
+                    });
+                  }}
+                  readOnly={!editAddress}
+                  disabled={!editAddress}
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <Button
+                  className="bg-rose-800 enabled:hover:bg-rose-900 mx-auto md:mx-0 rounded-full md:rounded-lg"
+                  onClick={() =>
+                    editAddress ? updateAddress() : setEditAddress(true)
+                  }
+                  disabled={loading}
+                >
+                  {editAddress ? "Update Address" : "Edit Address"}
+                </Button>
+              </div>
             </div>
           </div>
         </div>
