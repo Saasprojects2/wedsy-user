31af672a4b2b461af2080c270a257e88f109148d
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-09 20:34:40 +0530

    Updated My Payments Pages

---
+++ pages/my-payments/index.js
@@ -0,0 +1,299 @@
+import UserProfileHeader from "@/components/layout/UserProfileHeader";
+import UserSidebar from "@/components/layout/UserSidebar";
+import { toProperCase } from "@/utils/text";
+import { Pagination, Select, Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { BsBell } from "react-icons/bs";
+
+export default function Payments({ user }) {
+  const [payments, setPayments] = useState([]);
+  const [paymentStats, setPaymentStats] = useState({
+    totalAmount: 0,
+    amountPaid: 0,
+    amountDue: 0,
+  });
+  const [loading, setLoading] = useState(false);
+  const [paymentType, setPaymentType] = useState("All");
+  const [paymentPage, setPaymentPage] = useState(1);
+  const [paymentPageSize, setPaymentPageSize] = useState(10);
+  const [paymentList, setPaymentList] = useState([]);
+  const [paymentTotalPages, setPaymentTotalPages] = useState(1);
+  const fetchPayments = () => {
+    setLoading(true);
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/payment`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        const { totalAmount, amountPaid, amountDue } = response;
+        setLoading(false);
+        setPayments(response.payments);
+        setPaymentStats({ totalAmount, amountPaid, amountDue });
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+  const initializeRazorpay = () => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = "https://checkout.razorpay.com/v1/checkout.js";
+
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+  const makePayment = async ({ order_id, amount }) => {
+    const res = await initializeRazorpay();
+    if (!res) {
+      alert("Razorpay SDK Failed to load");
+      return;
+    }
+    var options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY, // Enter the Key ID generated from the Dashboard
+      name: "Wedsy",
+      currency: "INR",
+      amount: amount,
+      order_id: order_id,
+      description: "Your Event Payment",
+      // image: "https://manuarora.in/logo.png",
+      handler: function (response) {
+        // Validate payment at server - using webhooks is a better idea.
+        // console.log(response);
+        // alert(response.razorpay_payment_id);
+        // alert(response.razorpay_order_id);
+        // alert(response.razorpay_signature);
+        UpdatePayment({ order_id, response });
+      },
+      prefill: {
+        name: user.name,
+        email: user.email,
+        contact: user.phone,
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  };
+  const UpdatePayment = ({ response, order_id }) => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/payment/${order_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ response }),
+    })
+      .then((response) => (response.ok ? response.json() : null))
+      .then((response) => {
+        if (response.message === "success") {
+          fetchPayments();
+        }
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+  useEffect(() => {
+    fetchPayments();
+  }, []);
+  useEffect(() => {
+    if (payments.length > 0) {
+      let temp_payment = payments.filter((i) =>
+        paymentType == "All"
+          ? true
+          : paymentType == "Complete" && i.status === "paid"
+          ? true
+          : paymentType === "Rejected" &&
+            ["expired", "canceled"].includes(i.status)
+          ? true
+          : paymentType === "Pending" &&
+            !["expired", "canceled", "paid"].includes(i.status)
+          ? true
+          : false
+      );
+      let temp_total = temp_payment.length;
+      let temp_total_pages = temp_total / paymentPageSize;
+      let temp_page = paymentPage < temp_total_pages ? paymentPage : 1;
+      let s = (temp_page - 1) * paymentPageSize;
+      let e = temp_page * paymentPageSize;
+      let temp_list = temp_payment.filter(
+        (_, index) => index >= s && index < e
+      );
+      setPaymentList(temp_list);
+    }
+  }, [payments, paymentPage, paymentPageSize, paymentType]);
+  return (
+    <>
+      <div className="flex flex-col bg-gray-100">
+        <UserProfileHeader display={"my-payments"} />
+        <div className="flex flex-col gap-3 px-8 md:px-36 mb-12 md:my-12">
+          <div className="flex flex-row justify-center items-center bg-white p-1 text-black mx-auto rounded-xl">
+            <span className="uppercase bg-black text-white px-8 py-2 rounded-lg">
+              Decor
+            </span>
+            <span className="uppercase px-8 py-2">Makeup</span>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 divide-y-2 md:divide-y-0 divide-black md:divide-x-2 mt-8 py-2 border-b-2 bg-white rounded-2xl px-8 py-6 mx-auto">
+            <div className="flex flex-col items-center justify-center gap-2 p-4">
+              <p className="text-lg font-medium uppercase text-black">
+                Total Bills
+              </p>
+              <p className="text-3xl font-medium">
+                {paymentStats.totalAmount.toLocaleString("en-IN", {
+                  maximumFractionDigits: 2,
+                  style: "currency",
+                  currency: "INR",
+                })}
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 text-green-500 p-4">
+              <p className="text-lg font-medium uppercase text-black">
+                Amount Paid
+              </p>
+              <p className="text-3xl font-medium ">
+                {paymentStats.amountPaid.toLocaleString("en-IN", {
+                  maximumFractionDigits: 2,
+                  style: "currency",
+                  currency: "INR",
+                })}
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 text-rose-900 p-4">
+              <p className="text-lg font-medium uppercase text-black">
+                Balance Amount
+              </p>
+              <p className="text-3xl font-medium ">
+                {paymentStats.amountDue.toLocaleString("en-IN", {
+                  maximumFractionDigits: 2,
+                  style: "currency",
+                  currency: "INR",
+                })}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 items-center justify-center py-2 px-6 bg-gray-300 text-lg">
+          <BsBell />
+          <span>
+            Last Payment made on{" "}
+            {new Date(payments.reverse()[0]?.createdAt).toLocaleDateString(
+              "en-GB",
+              {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+              }
+            )}
+            <span className="font-semibold ml-6">
+              {(payments.reverse()[0]?.amount / 100).toLocaleString("en-IN", {
+                maximumFractionDigits: 2,
+                style: "currency",
+                currency: "INR",
+              })}
+            </span>
+          </span>
+        </div>
+        <div className="hidden md:flex flex-col gap-3 px-8 md:px-36 mb-12 md:my-12">
+          <p className="text-md">Payment History</p>
+          <div className="flex flex-row gap-6 items-center">
+            {["All", "Complete", "Pending", "Rejected"].map((i) => (
+              <div
+                key={i}
+                className={`rounded-full border bg-white py-2 px-8 cursor-pointer ${
+                  paymentType == i ? "border-black" : "border-white"
+                }`}
+                onClick={() => {
+                  setPaymentType(i);
+                  setPaymentPage(1);
+                }}
+              >
+                {i}
+              </div>
+            ))}
+          </div>
+          <div className="bg-white rounded-2xl flex-col flex gap-3 shadow-xl overflow-hidden">
+            <div className="width-full overflow-x-auto">
+              <Table hoverable className="width-full overflow-x-auto">
+                <Table.Head>
+                  <Table.HeadCell>Payment ID</Table.HeadCell>
+                  <Table.HeadCell>Date</Table.HeadCell>
+                  <Table.HeadCell>Amount</Table.HeadCell>
+                  <Table.HeadCell>Payment Mode</Table.HeadCell>
+                  <Table.HeadCell>Status</Table.HeadCell>
+                  <Table.HeadCell>Invoice</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                  {paymentList?.map((item, index) => (
+                    <Table.Row
+                      className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                      key={item._id}
+                    >
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                        {item.razporPayId}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {new Date(item.createdAt).toLocaleDateString("en-US", {
+                          day: "numeric",
+                          month: "short",
+                          year: "numeric",
+                        })}
+                      </Table.Cell>
+                      <Table.Cell className="font-medium">
+                        ₹{item.amount / 100}
+                      </Table.Cell>
+                      <Table.Cell className="">Payment Mode</Table.Cell>
+                      <Table.Cell className="">
+                        {toProperCase(item.status.split("_").join(" "))}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {toProperCase(
+                          item?.transactions[0].method?.split("_").join(" ")
+                        )}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+            <div className="p-4 flex flex-row justify-between items-center">
+              <Select
+                value={paymentPageSize}
+                onChange={(e) => {
+                  setPaymentPageSize(e.target.value);
+                }}
+                disabled={loading}
+              >
+                <option value={10}>10</option>
+                <option value={25}>25</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+              </Select>
+              <Pagination
+                currentPage={paymentPage}
+                layout="pagination"
+                nextLabel=""
+                onPageChange={(newPage) => {
+                  setPaymentPage(newPage);
+                }}
+                previousLabel=""
+                showIcons
+                totalPages={paymentTotalPages}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
