a35ad95f22df4e3f7901f000cb0d336f50c155f7
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-15 20:00:21 +0530

    Updated PlanYourEvent

--- components/screens/PlanYourEvent.js
+++ components/screens/PlanYourEvent.js
@@ -5,23 +5,25 @@ import styles from "./PlanYourEvent.module.css";
 export default function PlanYourEvent() {
   return (
     <>
-      <div className="hidden md:block mb-12">
+      <div className="mb-12">
         <div
-          className={`${styles.bg_div_top} flex pb-8 flex-col justify-end px-24 `}
+          className={`${styles.bg_div_top} flex pb-4 md:pb-8 flex-col justify-end px-6 md:px-24 text-center md:text-left`}
         >
-          <p className="font-medium text-xl text-white uppercase">
+          <p className="font-medium text-base md:text-xl text-white uppercase">
             Explore the ease of planning
           </p>
-          <p className="font-semibold text-3xl text-white">
+          <p className="font-semibold text-base md:text-3xl text-white uppercase">
             PLAN YOUR EVENT NOW WITH OUR NEW
           </p>
         </div>
         <div
-          className={`${styles.bg_div_bottom} lex pt-8 flex-col justify-start px-24`}
+          className={`${styles.bg_div_bottom} lex pt-4 md:pt-8 flex-col justify-start px-6 md:px-24 text-center md:text-left`}
         >
-          <p className="font-bold text-6xl text-[#A00000]">EVENT TOOL!</p>
-          <div className="flex flex-row justify-between items-center">
-            <p className="font-medium text-xl text-[#A00000] w-1/2">
+          <p className="font-semibold md:font-bold text-5xl md:text-6xl text-[#A00000]">
+            EVENT TOOL!
+          </p>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+            <p className="font-medium text-base md:text-xl text-[#A00000] md:w-1/2">
               Utilize the tool to shortlist and choose your decorations
               effortlessly - all in one place, at Wedsy.
             </p>
@@ -34,54 +36,6 @@ export default function PlanYourEvent() {
           </div>
         </div>
       </div>
-      <section className="w-full p-6 md:px-16 py-12 md:hidden">
-        <p className="font-semibold text-2xl md:text-4xl mb-12">
-          PLAN YOUR EVENT NOW WITH OUR NEW EVENT TOOL!
-        </p>
-        <div className="flex gap-6 relative">
-          <div className="flex flex-col gap-2 md:gap-8 text-sm md:text-lg">
-            <p className="text-xl w-3/4">
-              Explore the ease of planning with our event tool at Wedsy.
-            </p>
-            <p className="text-xl w-3/4">
-              Utilize the tool to shortlist and choose your decorations
-              effortlessly - all in one place, at Wedsy.
-            </p>
-            <Link
-              href={`/event`}
-              className="hidden md:inline bg-rose-900 rounded-full p-1 px-16 text-white w-max mx-auto mt-auto mb-12"
-            >
-              Try Now!
-            </Link>
-          </div>
-          <Image
-            src="/assets/images/event-tool.png"
-            alt="Decor"
-            width={0}
-            height={0}
-            sizes="100%"
-            style={{ width: "20%", height: "auto", marginLeft: "auto" }}
-            className="hidden md:inline"
-          />
-          <div className="inline md:hidden">
-            <Image
-              src="/assets/images/event-tool.png"
-              alt="Decor"
-              width={0}
-              height={0}
-              sizes="100%"
-              style={{ width: "100%" }}
-              className=""
-            />
-          </div>
-        </div>
-        <Link
-          href={`/event`}
-          className="block md:hidden bg-rose-900 rounded-full p-1 px-16 text-white w-max mx-auto mt-4 md:mt-auto mb-4 md:mb-12"
-        >
-          Try Now!
-        </Link>
-      </section>
     </>
   );
 }
