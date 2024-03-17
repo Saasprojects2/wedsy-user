f38f58b861766b92cf829103485b031b90c8f799
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-17 23:53:16 +0530

    Updated Event Planner

--- pages/event/[event_id]/planner.js
+++ pages/event/[event_id]/planner.js
@@ -4,7 +4,7 @@ import DecorItemsList from "@/components/event-tool/DecorItemsList";
 import DecorPackagesList from "@/components/event-tool/DecorPackagesList";
 import EventDayInfo from "@/components/event-tool/EventDayInfo";
 import EventSummaryTable from "@/components/event-tool/EventSummaryTable";
-import EventToolShareButton from "@/components/event-tool/EventToolShareButton";
+import EventToolHeader from "@/components/event-tool/EventToolHeader";
 import EventToolSidebar from "@/components/event-tool/EventToolSidebar";
 import MandatoryItemsList from "@/components/event-tool/MandatoryItemsList";
 import NotesModal from "@/components/event-tool/NotesModal";
@@ -13,8 +13,6 @@ import { toPriceString } from "@/utils/text";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { AiOutlinePlusSquare } from "react-icons/ai";
-import { BsArrowLeft } from "react-icons/bs";
 
 export default function EventTool({ user }) {
   const divRef = useRef(null);
@@ -246,71 +244,14 @@ export default function EventTool({ user }) {
         event_id={event_id}
       />
       <div className="flex flex-col overflow-hidden hide-scrollbar">
-        {/* Event Planner Header */}
-        <div className="md:bg-[#DBB9BD] md:px-8 flex-wrap flex flex-col md:flex-row gap-0 md:gap-4 items-center justify-evenly font-medium text-center text-lg text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-          <div className="text-black font-medium px-8 md:px-0 py-4 md:py-0 md:pr-8 text-left flex flex-row justify-between items-center w-full md:w-auto">
-            <Link href={`/event/${event_id}`} className="md:hidden mr-auto">
-              <BsArrowLeft size={24} />
-            </Link>
-            <span className="mr-auto md:mr-0">{event.name}</span>
-            <EventToolShareButton
-              eventName={event?.name}
-              eventId={event?._id}
-            />
-          </div>
-          <div className="hidden md:flex md:flex-row gap-6 mx-auto">
-            {event?.eventDays?.map((item, index) => (
-              <div
-                key={item._id}
-                className={`hidden md:block px-8 mx-1 py-2 my-2 cursor-pointer ${
-                  eventDay === item._id
-                    ? " font-semibold bg-white rounded-2xl text-rose-900"
-                    : "font-normal text-black"
-                }`}
-                onClick={() => {
-                  setEventDay(item._id);
-                }}
-              >
-                {item.name}
-              </div>
-            ))}
-          </div>
-          <div className="overflow-hidden md:hidden w-full">
-            <div className="flex md:hidden flex-row gap-3 w-full overflow-x-auto mb-3 items-center">
-              {event?.eventDays?.map((item, index) => (
-                <div
-                  key={item._id}
-                  className={`md:hidden px-6 mx-1 py-2 cursor-pointer text-xs shadow-md rounded-full font-semibold ${
-                    eventDay === item._id
-                      ? "bg-rose-900 text-white"
-                      : "text-black bg-gray-300"
-                  }`}
-                  onClick={() => {
-                    setEventDay(item._id);
-                  }}
-                >
-                  {item.name}
-                </div>
-              ))}
-              {!event?.status?.approved && (
-                <AiOutlinePlusSquare
-                  size={24}
-                  onClick={() => setAddEventDayModalOpen(true)}
-                  cursor={"pointer"}
-                  className="ml-auto mr-6"
-                />
-              )}
-            </div>
-          </div>
-          {!event?.status?.approved && (
-            <AiOutlinePlusSquare
-              size={24}
-              onClick={() => setAddEventDayModalOpen(true)}
-              cursor={"pointer"}
-              className="hidden md:block"
-            />
-          )}
-        </div>
+        <EventToolHeader
+          event={event}
+          setEventDay={setEventDay}
+          eventDay={eventDay}
+          setAddEventDayModalOpen={setAddEventDayModalOpen}
+          allowEdit={true}
+          event_id={event_id}
+        />
         <div
           className="grid md:grid-cols-4 gap-6 py-4 overflow-hidden hide-scrollbar grow"
           ref={divRef}
