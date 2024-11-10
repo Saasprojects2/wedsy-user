b9829a7e369dee649d08ebe447212f2863c25f6c
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-11-10 23:52:11 +0530

    Added Google Tags

--- pages/_app.js
+++ pages/_app.js
@@ -149,6 +149,25 @@ function App({ Component, pageProps }) {
           name="google-site-verification"
           content="0rZGstdf8ZBfbVvggmIOXJAq-5kDHnKwFM8bRpUnRKU"
         />
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KH7RV8NN');
+              `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+        {/* Google Ads */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5861954260216854"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       {/* <!-- Google tag (gtag.js) --> */}
       <Script
@@ -169,6 +188,16 @@ function App({ Component, pageProps }) {
         `,
         }}
       />
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KH7RV8NN"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
       {loading ? (
         <div className="grid place-content-center h-screen ">
           <Spinner size="xl" />
